b799a9ae3fd362cce9ba8da7539cd4e3d0996ef1
IT Resident Engineer <ir-jag.ithelpdesk@iipl.com>
2026-05-20 10:45:49 +0530

    render deployment fix

--- app.py
+++ app.py
@@ -432,9 +432,4 @@ def excel(id):
 # RUN APP
 # =========================
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=10000)
